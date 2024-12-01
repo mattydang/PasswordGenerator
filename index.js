4ca585114e2bafa26643fbc1f91d90a6e1ddeeb5
mattydang <matthewdang1410@gmail.com>
2024-12-01 14:49:49 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+function genPassword(length, includeLowercase, includeUppercase, includeNumbers,includeSymbols){
+
+    const lowercaseChars = "abcdefghijklmnopqrstuvwyz";
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWYZ";
+    const numberChars = "0123456789";
+    const symbolChars = "!@$#%^&*()_+=-";
+
+    let allowedChars = "";
+    let password = '';
+
+    allowedChars += includeLowercase ? lowercaseChars : "";
+    allowedChars += includeUppercase ? uppercaseChars : "";
+    allowedChars += includeNumbers ? numberChars : "";
+    allowedChars += includeSymbols ? symbolChars : "";
+
+    if(length <= 0){
+        return `(password length must be at least 1)`;
+    }
+    if(allowedChars.length === 0){
+        return `(At least 1 set of character need to be selected)`;
+    }
+    for(let i = 0; i < length; i++){
+        const randomIndex = Math.floor(Math.random() * allowedChars.length);
+        password += allowedChars(randomIndex);
+    }
+    return '';
+}
+
+const passwordLength = 12;
+const includeLowercase = true;
+const includeUppercase = true;
+const includeNumbers = true;
+const includeSymbols = true;
+
+const password = genPassword(passwordLength, 
+                            includeLowercase, 
+                            includeUppercase, 
+                            includeNumbers, 
+                            includeSymbols);
